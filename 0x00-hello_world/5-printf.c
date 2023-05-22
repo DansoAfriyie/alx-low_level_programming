0365ed6a3d551859abd2570c2ed8148c4bf3cc36
DansoAfriyie <dansoafriyie778@gmail.com>
2023-05-22 13:20:33 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This program will print a string of characters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
