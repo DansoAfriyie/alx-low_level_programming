e161c5b5d8ce272e7759156c41cbad709b6b9c47
DansoAfriyie <dansoafriyie778@gmail.com>
2023-05-22 13:11:31 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program to print a string is characters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
