c0984e2d0d42a0df4db4deff117d267d12e428d7
DansoAfriyie <dansoafriyie778@gmail.com>
2023-05-23 05:13:49 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Printing all single numbers of base 10 starting from 0
+ *
+ * Return: (Success) 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	printf("\n");
+	return (0);
+}
+
